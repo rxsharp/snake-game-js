5325f9d2a9815586286c1767d09fda24dc580280
Rick G <lacunainc04@gmail.com>
2015-09-15 21:59:02 -0700

    Complete Lesson 3

--- snake.js
+++ snake.js
@@ -1,5 +1,9 @@
-var snake = [{top: 0, left: 0 }];
 
-var drawableSnake = {color: "#3EBE9B", pixels: snake};
+var drawSnake = function(snakeToDraw) {
+var drawableSnake = {color: "#3EBE9B", pixels: snakeToDraw};
 var drawableObjects = [drawableSnake];
 CHUNK.draw(drawableObjects);
+}
+
+var snake = [{top: 0, left: 0 }];
+drawSnake(snake);
