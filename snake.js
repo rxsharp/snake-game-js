a1d1c9d2c2339e0276adc5af2ba21824e28183ae
Rick G <lacunainc04@gmail.com>
2015-09-17 16:54:07 -0700

    Complete Lesson 6 Add two new classes, .add and .remove to notes.css. Change start position to test directions and potential initial position.

--- snake.js
+++ snake.js
@@ -5,9 +5,23 @@ var drawableObjects = [drawableSnake];
 CHUNK.draw(drawableObjects);
 }
 
+var moveSegment = function(segment) {
+  if (segment.direction === "down") {
+    return { top: segment.top + 1, left: segment.left }
+  } else if (segment.direction === "up") {
+    return { top: segment.top - 1, left: segment.left }
+  } else if (segment.direction === "right") {
+    return { top: segment.top, left: segment.left + 1 }
+  } else if (segment.direction === "left") {
+    return { top: segment.top, left: segment.left - 1 }
+  }
+  return segment;
+}
+
 var moveSnake = function(snake) {
-	oldSegment = snake[0];
-	newSegment = {top: oldSegment.top + 1, left: oldSegment.left};
+	var oldSegment = snake[0];
+	var newSegment = moveSegment(oldSegment);
+	newSegment.direction = oldSegment.direction;
 	newSnake = [newSegment];
 	return newSnake
 }
@@ -18,7 +32,7 @@ drawSnake(snake);
 }
 
 // Snake start position
-var snake = [{top: 0, left: 0 }];
+var snake = [{top: 5, left: 9, direction: "up" }];
 
 //Execution of snake movement and draw commands
 CHUNK.executeNTimesPerSecond(advanceGame, 1);
