996cea673495abc2bd6e95f687482221367fe4d7
Rick G <lacunainc04@gmail.com>
2015-09-16 21:07:00 -0700

    Complete Lesson 4

--- snake.js
+++ snake.js
@@ -5,5 +5,11 @@ var drawableObjects = [drawableSnake];
 CHUNK.draw(drawableObjects);
 }
 
+var moveSnake = function(snake) {
+	oldSegment = snake[0];
+	newSegment = {top: oldSegment.top + 1, left: oldSegment.left};
+	newSnake = [newSegment];
+	return newSnake
+}
 var snake = [{top: 0, left: 0 }];
 drawSnake(snake);
